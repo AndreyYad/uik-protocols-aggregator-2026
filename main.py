7aa786995b7c6176de5c9abde4c64ecd559fd8ef
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2026-09-22 22:57:28 +0300

    Дообавил Main

---
+++ main.py
@@ -0,0 +1,9 @@
+from get_requests.get_requests import Requests
+
+class Main:
+    def __init__(self):
+        uiks = Requests.getDistrictData(209)
+        print(region_data)
+
+if __name__ == "__main__":
+    Main()

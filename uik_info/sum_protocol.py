350e07ce548f09851b653eddcdbd465cca372c8c
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2026-09-24 16:17:19 +0300

    Сделал округление суммарной явки

--- uik_info/sum_protocol.py
+++ uik_info/sum_protocol.py
@@ -1,8 +1,13 @@
 from .protocol import Protocol
 class SumProtocol(Protocol):
-    def __init__(self, list_protocols: list(Protocol)):
+    def __init__(self, list_protocols: list[Protocol]):
         self.list_protocols = list_protocols
-        super().__init__([
+
+        sum_protocol = [
             sum(columns) if all(isinstance(x, (int, float)) for x in columns) else ""
             for columns in zip(*[p.protocol for p in list_protocols])
-        ])
+        ]
+
+        sum_protocol[12] = round(sum_protocol[12] / len(list_protocols), 3)
+
+        super().__init__(sum_protocol)

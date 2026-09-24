0202a0eabc8d3dcd4ed7ba7a6633691c45524a4f
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2026-09-24 16:17:50 +0300

    Обновил Uiks

--- uik_info/uiks.py
+++ uik_info/uiks.py
@@ -3,17 +3,22 @@ from fontTools.varLib.instancer import names
 from uik_info.candidates import Candidates
 from uik_info.candidate import Candidate
 from uik_info.uik import Uik
+from uik_info.sum_protocol import SumProtocol
 
 class Uiks():
-    def __init__(self, list_uiks_data: list[dict]):
+    def __init__(self, list_uiks_data: list[dict] | None):
+        if list_uiks_data is None:
+            return
+
         self.list_uiks: list[Uik] = []
+        self.sum_protocol: SumProtocol
         self.candidate_full: dict[str, list[int]] = {}
         count = 0
 
         for uik_data in list_uiks_data:
             self.list_uiks.append(Uik(uik_data))
 
-        self.sortCandidates()
+        self.sum_protocol = SumProtocol([uik.getProtocol() for uik in self.list_uiks])
 
         for candidate_uik_info in [
             candidate
@@ -29,6 +34,8 @@ class Uiks():
         for name in self.candidate_full.keys():
             self.candidate_full[name] = sum(self.candidate_full[name])
 
+        self.sortCandidates()
+
     def sortCandidates(self):
         sorted_names = self.getSortsNames()
 
@@ -43,6 +50,12 @@ class Uiks():
 
     def getListUiks(self) -> list[Uik]:
         return self.list_uiks
+
+    def getSumProtocol(self) -> SumProtocol:
+        return self.sum_protocol
+
+    def getCandidatesFull(self) -> dict[str, int]:
+        return self.candidate_full
 
     def getSortsNames(self) -> list[str]:
         names = sorted(self.candidate_full, key=self.candidate_full.get, reverse=True)

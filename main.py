77aa4e2a02752ce1b40796fb39ea352ee4c4cc6b
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2026-09-25 11:35:10 +0300

    Сделал при запуске программы необходимость ввода в консоль необходимых округов

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from get_requests.get_requests import Requests
 from uik_info.uiks import Uiks
 from excel_file.table import Table
+import re
 
 class Main:
     def __init__(self):
         list_error_district = []
-        for district in range(13, 226):
+
+        print(
+            'Введите какой(ие) округ(а) вам нужны\n',
+            'В формате:\n',
+            'Пример одного округа: 1\n',
+            'Пример нескольких 3-7\n'
+        )
+
+        while True:
+            enter = input('Ввод: ')
+            if re.fullmatch('\d+', enter) and 0 < int(enter) <= 225:
+                list_districts_num = [int(enter)]
+                break
+            if re.fullmatch('\d+-\d+', enter):
+                num1, num2 = [int(num_str) for num_str in re.split('-', enter)]
+                if not(0 < num1 <= 225 and 0 < num2 <= 225):
+                    print('Всего 225 округов - поэтому число должно ыть в иапозоне от 1 до 225')
+                elif num1 >= num2:
+                    print('Первое число должно быть меньше второго')
+                else:
+                    list_districts_num = range(num1, num2 + 1)
+                    break
+
+        for district in list_districts_num:
             print(f"Генерация таблицы по {district} округу")
             if self.bornDistrictTable(district):
                 print(f"Генерация таблицы по {district} округу прошла успешно")
@@ -24,6 +48,7 @@ class Main:
             table = Table(district)
 
             table.setUiks(Uiks(request))
+
             table.paint()
 
             table.createList("Партии")
